53dbb82b6707d7789d978b3852919d08d09a1fff
washalbano <washalbano@gmail.com>
2022-10-09 14:10:11 -0300

    feat: add mounted query example

--- api/index.php
+++ api/index.php
@@ -1,5 +1,17 @@
 <?php
 if (sizeof($_POST)) {
-  die(json_encode(['Resposta do arquivo .php', '_POST' => $_POST, '_FILES' => $_FILES]));
+
+  extract($_POST);
+
+  // montar uma query
+  $query = "insert into table(title, description, file_name) values ('$title', '$description', '{$_FILES['photo']['name']}')";
+
+  die(json_encode([
+    'Resposta do arquivo .php',
+    '_POST' => $_POST,
+    '_FILES' => $_FILES,
+    'query' => $query
+
+  ]));
 }
 die(json_encode('Nada foi postado.'));
